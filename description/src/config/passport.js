ec3434ba1d821852ae7ee03c52b8a3e2e94f33bf
Sampaguitas <timothee.desurmont@sampaguitas.com>
2021-02-24 18:09:37 +0400

    changes

--- description/src/config/passport.js
+++
@@ -1,9 +0,0 @@
-const JwtStrategy = require("passport-jwt").Strategy;
-const ExtractJwt = require("passport-jwt").ExtractJwt;
-
-module.exports = passport => {
-    passport.use(new JwtStrategy({
-        "jwtFromRequest": ExtractJwt.fromAuthHeaderAsBearerToken(),
-        "secretOrKey": require("./keys").secret
-    }, (jwt_payload, done) => done(null, jwt_payload)));
-};
