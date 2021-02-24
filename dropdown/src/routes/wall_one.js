a084fbff0abfd49faddd0d969f15dfae1dbf018e
Sampaguitas <timothee.desurmont@sampaguitas.com>
2021-02-24 14:10:07 +0400

    changes

--- dropdown/src/routes/wall_one.js
+++ dropdown/src/routes/wall_one.js
@@ -1,59 +1,74 @@
 const express = require("express");
 const router = express.Router();
 
+let regMm = /^(\d|\.)* mm$/
+let regIn = /^(\d|\.)* in$/
+let regIdt = /^(STD|XS|XXS)$/
+
 router.get("/", (req, res) => {
     
-    const steel_type = decodeURI(req.query.steel_type);
-    const pff_type = decodeURI(req.query.pff_type);
-    const size_one = decodeURI(req.query.size_one);
+    const steelType = decodeURI(req.query.steel_type);
+    const pffType = decodeURI(req.query.pff_type);
+    const sizeOne = decodeURI(req.query.size_one);
+    let regSch = [ "CARBON_STEEL", "LOW_TEMP", "LOW_ALLOY" ].includes(steelType) ? /^S\d*$/ : /^S\d*S?$/
+    let noWallOne = [ "FORGED_FITTINGS", "MI_FITTINGS", "SW_GASKETS", "FASTENERS", "RING_GASKETS" ];
 
-    let regMm = /^(\d|\.)* mm$/
-    let regIn = /^(\d|\.)* in$/
-    let regIdt = /^(STD|XS|XXS)$/
-    let regSch = [
-        "CARBON_STEEL",
-        "LOW_TEMP",
-        "LOW_ALLOY",
-    ].includes(steel_type) ? /^S\d*$/ : /^S\d*S?$/
-
-    let noWallOne = [
-        "FORGED_FITTINGS",
-        "MI_FITTINGS",
-        "SW_GASKETS",
-        "FASTENERS",
-        "RING_GASKETS"
-    ]
-
-    let sizeObject = require("../constants/sizes.json").find(e => e.tags.includes(size_one) && !!e.mm)
-    if (noWallOne.includes(pff_type) || !sizeObject) {
+    if (noWallOne.includes(pffType) || ["undefined", "OTHERS", ""].includes(sizeOne)) {
         res.status(200).json([])
     } else {
-        let temp = require("../constants/walls.json")
-        .filter(e => e.sizeId === sizeObject.mm)
-        .reduce(function (acc, cur) {
-            cur.tags.map(tag => {
-                if(regMm.test(tag)) {
-                    acc.mm.push(tag);
-                } else if(regIn.test(tag)) {
-                    acc.in.push(tag);
-                } else if(regIdt.test(tag)) {
-                    acc.idt.push(tag);
-                } else if(regSch.test(tag)) {
-                    acc.sch.push(tag);
-                }
-            });
-            return acc;
-        }, {
-            "mm": [],
-            "in": [],
-            "idt": [],
-            "sch": [],
+        require("../models/Size").findOne({ tags: sizeOne, mm: { $ne: null } }, function(errTempOne, resTempOne) {
+            if (errTempOne || !resTempOne) {
+                res.status(200).json([]);
+            } else {
+                require("../models/Wall").aggregate([
+                    {
+                        $unwind: "$tags"
+                    },
+                    {
+                        $match: {
+                            sizeId: resTempOne.mm
+                        }
+                    },
+                    {
+                        $sort: {
+                            "_id" : 1
+                        }
+                    },
+                    {
+                        $group : {
+                            _id : "$key", tags : { $push : "$tags" } 
+                        }
+                    }
+                ])
+                .exec(function(error, result) {
+                    if(!!error || result.length < 1) {
+                        res.status(200).json([]);
+                    } else {
+                        let temp = result[0].tags.reduce(function (acc, cur) {
+                            if(regMm.test(cur)) {
+                                acc.mm.push(cur);
+                            } else if(regIn.test(cur)) {
+                                acc.in.push(cur);
+                            } else if(regIdt.test(cur)) {
+                                acc.idt.push(cur);
+                            } else if(regSch.test(cur)) {
+                                acc.sch.push(cur);
+                            }
+                            return acc;
+                        }, {
+                            "mm": [],
+                            "in": [],
+                            "idt": [],
+                            "sch": [],
+                        });
+                        res.status(200).json(
+                            [...temp.idt, ...temp.sch, ...temp.mm, ...temp.in]
+                            .filter((value, index, self) => self.indexOf(value) === index)
+                        );
+                    }
+                });
+            }
         });
-
-        res.status(200).json(
-            [...temp.idt, ...temp.sch, ...temp.mm, ...temp.in]
-            .filter((value, index, self) => self.indexOf(value) === index)
-        );
     }
 });
 
