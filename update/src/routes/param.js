442457d2245873d6d8e32b106eddc35b6f13f9c6
Sampaguitas <timothee.desurmont@sampaguitas.com>
2021-02-28 11:11:49 +0400

    changes

--- update/src/routes/param.js
+++ update/src/routes/param.js
@@ -2,22 +2,31 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", (req, res) => {
-    const { artNr } = req.body;
-    const { sizeOne, sizeTwo, sizeThree, wallOne, wallTwo, type, grade, length, end, surface } = req.body;
-    require("../functions/getParam")(sizeOne, sizeTwo, sizeThree, wallOne, wallTwo, type, grade, length, end, surface)
-    .then(parameters => {
-        require("../models/Param").findOneAndUpdate({ "artNr": artNr }, {
-            "description": require("../functions/getDesc")(parameters),
-            "vlunar": require("../functions/getVlunar")(parameters),
-            "parameters": parameters
-        }, {new: true, upsert: true }, function(errUpdate, resUpdate) {
-            if (!!errUpdate || !resUpdate) {
-                res.status(400).json({message: "Could not update param."});
-            } else {
-                res.status(200).json({message: resUpdate });
-            }
+    const { artNr, sizeOne, sizeTwo, sizeThree, wallOne, wallTwo, type, grade, length, end, surface } = req.body;
+    if (!artNr || !sizeOne || !type || !grade) {
+        res.status(400).json({ "message": "description is incomplete." });
+    } else {
+        require("../functions/getParam")(sizeOne, sizeTwo, sizeThree, wallOne, wallTwo, type, grade, length, end, surface)
+        .then(parameters => {
+            require("../models/Param").findOneAndUpdate({
+                "artNr": artNr
+            },
+            {
+                "description": require("../functions/getDesc")(parameters),
+                "vlunar": require("../functions/getVlunar")(parameters),
+                "parameters": parameters
+            },
+            {
+                new: true, upsert: true 
+            }, function(errUpdate, resUpdate) {
+                if (!!errUpdate || !resUpdate) {
+                    res.status(400).json({message: "Could not update param."});
+                } else {
+                    res.status(200).json({message: resUpdate });
+                }
+            });
         });
-    });
+    }
 });
 
 module.exports = router;
